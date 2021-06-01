98c7399bdd82fc4024ce27780e0e1464dce6a535
Laurel Stickney <ls60@protonmail.com>
2021-06-01 10:54:43 -0700

    commented section removed

--- src/database/setup.js
+++ src/database/setup.js
@@ -1,22 +1,5 @@
 // SET UP DATABASE
 
-// const mongoose = require('mongoose')
-
-// const url = 'mongodb+srv://laurel:mernstack@cluster0.mm9cx.mongodb.net/EventsDB?retryWrites=true&w=majority'
-// const connectionParams={
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true 
-// }
-// mongoose.connect(url,connectionParams)
-//     .then( () => {
-//         console.log('Connected to database ')
-//     })
-//     .catch( (err) => {
-//         console.error(`Error connecting to the database. \n${err}`);
-//     })
-
-
 const mongoose = require('mongoose')
 
 const url = 'mongodb+srv://laurel:mernstack@cluster0.mm9cx.mongodb.net/EventsDB?retryWrites=true&w=majority'
